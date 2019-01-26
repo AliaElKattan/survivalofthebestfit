ede677bf590a8e5f73da52f1d3a1f3280355b551
mklasinc <mk4908@nyu.edu>
2019-01-26 21:43:00 +0100

    remove the unchosen choice button

--- game/public/components/demo/demo.js
+++ game/public/components/demo/demo.js
@@ -5,8 +5,19 @@ Array.from(buttons).forEach(function(element) {
   
   element.addEventListener('click', (e) => {
     const choiceButton = e.target;
+    // add a 'chosen' class tag to the button
     choiceButton.classList.add('choiceButton--chosen');
+    // get current replica and step
     const currentReplica = choiceButton.closest(".replica");
+    const currentStep = currentReplica.dataset.step;
+    // hide the other choice button
+    const choiceButtons = document.querySelectorAll('.choiceButton[data-step="' + currentStep + '"]');
+    choiceButtons.forEach(button => {
+      if (!button.classList.contains('choiceButton--chosen')){
+        button.closest('.choiceButton__wrapper').classList.add('is-inactive');
+      };
+    });
+    // choiceButton.classList.add('js-active');
     
     const nextReplica = (() => {
       try {
