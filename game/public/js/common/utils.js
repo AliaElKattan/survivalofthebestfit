72172a02994c3f96d504e3ae3481159ebe644c56
mklasinc <mk4908@nyu.edu>
2019-01-29 20:46:00 +0100

    added array support to the uv2px function

--- game/public/js/common/utils.js
+++ game/public/js/common/utils.js
@@ -17,6 +17,7 @@ var spacingUtils = {
 
 const uv2px = (uv, axis = null) => {
   const { width, height } = pixiApp.screen;
+  // input is object
   if (typeof uv === 'object' && uv !== null) {
     const scaled_x = uv.x !== undefined ? uv.x*width : null;
     const scaled_y = uv.y !== undefined ? uv.y*height : null;
@@ -24,10 +25,14 @@ const uv2px = (uv, axis = null) => {
       x: scaled_x,
       y: scaled_y
     };
+  // input is number + axis
   } else if (typeof uv === 'number' && axis !== null && /^(w|h)$/.test(axis)) {
     return axis === 'w' ? uv*width : uv*height;
+  // input is array
+  } else if (Array.isArray(uv)) {
+    return [uv[0]*width, uv[1]*height];
   } else {
-    throw "You supplied an invalid value, we need an object: {x: x_val, y: y_val} or value and axis (val, 'h')";
+    throw "You supplied an invalid value to the function, check utils file for valid inputs";
   };
 };
 
