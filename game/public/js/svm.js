02e61bf4ea2005f2a0a0fad628a55b2cf3eb92b8
Gabor <gabor.csapo@nyu.edu>
2019-02-02 15:00:35 +0000

    SVM experiments

--- game/public/js/svm.js
+++ game/public/js/svm.js
@@ -1,6 +1,4 @@
 var svm;
-const features1 = [[0, 0], [1, 1], [1, 0], [0, 1]];
-const labels1 = [0, 0, 1, 1];
 
 async function trainSVM(features, labels) {
     const SVM = await libsvm;
@@ -8,9 +6,52 @@ async function trainSVM(features, labels) {
     svm.train(features, labels);
 }
 
-trainSVM(features1, labels1).then(() => {
-  var predictedLabel = svm.predictOne([0.7, 0.8]);
-  console.log(predictedLabel);
+
+
+function createRandomCv(){
+    var label = Math.floor(Math.random() * 3);
+    var features = [Math.sqrt(Math.random()), Math.sqrt(Math.random()), Math.sqrt(Math.random()), Math.sqrt(Math.random())]
+    var cv = {"label": label, "features": features}
+    var scale = cv["label"]/cv["features"].reduce((a, b) => a + b, 0);
+    cv["features"] = cv["features"].map(feature => feature * scale);
+    cv["features"].push(Math.floor(Math.sqrt(Math.sqrt(Math.random()) * 3)));;
+
+    return cv
+}
+
+var cvs = [];
+for (var i = 0; i < 100; i++) {
+    cvs.push(createRandomCv());
+}
+
+var counter = {0:0, 1:0, 2:0};
+var skillLevel = {0:0, 1:0, 2:0};
+var features = []
+var labels = []
+for (var cv in cvs) {
+    counter[cvs[cv]["features"][4]]++;
+    skillLevel[cvs[cv]["label"]]++;
+    features.push(cvs[cv]["features"]);
+    labels.push(cvs[cv]["label"]);
+}
+console.log(counter, skillLevel);
+
+trainSVM(features, labels).then(() => {
+    runTest([1, 1, 1, 1, 0])
+    runTest([1, 1, 1, 1, 2])
 });
 
-export { trainSVM };
+function runTest(input){
+
+    var samples = new Array(100).fill(input);
+    var counts = {};
+    var arr = svm.predict(samples);
+    for (var i = 0; i < arr.length; i++) {
+      var num = arr[i];
+      counts[num] = counts[num] ? counts[num] + 1 : 1;
+    }
+    console.log(counts)
+}
+
+
+export { trainSVM, };
