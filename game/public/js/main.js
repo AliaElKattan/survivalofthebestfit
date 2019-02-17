16bc0b022c29b625931a6746a8afb665db02a8d9
mklasinc <mk4908@nyu.edu>
2019-02-17 18:26:24 +0100

    added CV UI component

--- game/public/js/main.js
+++ game/public/js/main.js
@@ -4,9 +4,12 @@ import { pixiApp, startTweenManager } from './shared.js';
 import { gameFSM } from './gameStates.js';
 import {trainSVM } from './svm.js';
 
+import ResumeUI from '../components/ui/ui-resume/ui-resume';
+
 console.log(txt.stageZero.welcome);
 console.log('hellooo!');
-// 
+
+// new ResumeUI({content: 'this is my CV', show: true}); 
 // eventEmitter.emit('emitter-test-emit', {});
 
 
