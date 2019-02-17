5d1f90cddd5e69e73eae8020964dce6dd9eddd15
mklasinc <mk4908@nyu.edu>
2019-02-17 11:31:28 +0100

    refactored text box UI elements to support relative sizes

--- game/public/js/main.js
+++ game/public/js/main.js
@@ -1,15 +1,11 @@
 import { eventEmitter } from './shared.js';
 import "@babel/polyfill";
 import { pixiApp, startTweenManager } from './shared.js';
-import TextBoxUI from '../components/ui/ui-instruction/ui-instruction';
 import { gameFSM } from './gameStates.js';
 import {trainSVM } from './svm.js';
 
 console.log(txt.stageZero.welcome);
 console.log('hellooo!');
-
-// const textboxUI = new TextBoxUI();
-// setTimeout(() => textboxUI.show(), 2000);
 // 
 // eventEmitter.emit('emitter-test-emit', {});
 
