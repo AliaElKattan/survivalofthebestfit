2203bb01ddcbcf52c192b0ce2ce303f2bcf4c494
jihjihk <jihyun@nyu.edu>
2019-03-10 13:59:33 +0800

    cv loading per person clicked

--- game/public/assets/text/cvCollection.js
+++ game/public/assets/text/cvCollection.js
@@ -26,13 +26,46 @@ const cvCollection = {
     ],
     smallOfficeStage: [
         {
-            name: 'Remi Gottlieb',
+            name: '0 Remi Gottlieb',
             qualifications: [5, 7, 8, 9],
         },
         {
-            name: 'Jane Doe',
+            name: '1 Jane Doe',
             qualifications: [10, 10, 10, 10],
         },
+
+        {
+            name: '2 Ellie-Louise Douglas',
+            qualifications: [10, 8, 10, 10],
+        },
+        {
+            name: '3 Suhail Brett',
+            qualifications: [7, 10, 7, 10],
+        },
+        {
+            name: '4 Jorgie Davie',
+            qualifications: [10, 10, 10, 10],
+        },
+        {
+            name: '5 Christos Knott',
+            qualifications: [9, 7, 6, 5],
+        },
+        {
+            name: '6 Ezra Burt',
+            qualifications: [10, 10, 10, 10],
+        },
+        {
+            name: '7 Franciszek Pugh',
+            qualifications: [10, 3, 2, 10],
+        },
+        {
+            name: '8 Wojciech Milner',
+            qualifications: [8, 6, 10, 4],
+        },
+        {
+            name: '9 Kobe Gough',
+            qualifications: [8, 4, 5, 1],
+        }
     ],
 };
 
