5612066419fdf063da39f715f8ffa6c5eb308e4e
mklasinc <mk4908@nyu.edu>
2019-03-17 11:14:03 +0100

    commented out on hover for ml lab view

--- game/public/components/pixi/ml/person.js
+++ game/public/components/pixi/ml/person.js
@@ -2,19 +2,19 @@ import {bluePersonTexture} from '../../../controllers/common/textures.js';
 import {yellowPersonTexture} from '../../../controllers/common/textures.js';
 
 export default class {
-    constructor({x, parent, personData}) {
+    constructor({x, parent, personData, id}) {
         this.personData = personData;
         this.person = this.personData.color === 'yellow' ? new PIXI.Sprite(yellowPersonTexture) : new PIXI.Sprite(bluePersonTexture);
         this.x = x;
         this.parentContainer = parent;
-        // this.id = id || 0;
+        this.id = id;
     }
 
     draw() {
         this.person.scale.set(0.2);
         this.person.x = this.x;
         this.person.y = - this.person.height/2;
-        // this.person.id = this.id;
+        this.person.id = this.id;
         this.person.anchor.set(0.5);
         // this.person.on('mouseover', onPersonHover);
         this.parentContainer.addChild(this.person);
@@ -24,9 +24,8 @@ export default class {
         return this.personData;
     }
 }
-// 
 // function onPersonHover(event) {
 //     eventEmitter.emit('ml-person-hovered', {});
 //     candidateInScope = this.id;
-//     console.log("Current Candidate ID: " + candidateInScope);
+//     console.log('Current Candidate ID: ' + candidateInScope);
 // }
