8a0431b9664e1444ddfe08fb18ea4900d11cfc21
mklasinc <mk4908@nyu.edu>
2019-03-19 10:58:20 +0100

    fixed layout issue with dataset view grid

--- game/public/components/interface/ml/person-card/person-card.js
+++ game/public/components/interface/ml/person-card/person-card.js
@@ -6,6 +6,7 @@ export default class {
         this.$acceptedDataset = $parent.find('#dataset-accepted');
         this.$rejectedDataset = $parent.find('#dataset-rejected');
         this.$template = $parent.find('#person-card-template');
+        this.$el = null;
         this.personData = resume.data;
         this.id = resume.data.id;
         console.log(this.id);
@@ -15,14 +16,14 @@ export default class {
     }
 
     createCard() {
-        const $card = this.$template
+        this.$el = this.$template
             .clone()
             .removeAttr('id')
             .attr('data-id', this.id)
-            .addClass(CLASSES.DATASET_GRID_ITEM)
-            .removeClass(CLASSES.IS_INACTIVE);
-        $card.find('.PersonCard-name').html(this.personData.name);
-        this.status === 'accepted' ? $card.appendTo(this.$acceptedDataset) : $card.appendTo(this.$rejectedDataset);
+            .addClass(CLASSES.DATASET_GRID_ITEM);
+        this.$el.find('.PersonCard-name').html(this.personData.name);
+        this.status === 'accepted' ? this.$el.appendTo(this.$acceptedDataset) : this.$el.appendTo(this.$rejectedDataset);
+        this.show();
     }
 
     getID() {
@@ -37,6 +38,13 @@ export default class {
 
     _addEventListeners() {
 
+    }
+
+    show() {
+        let cardClass = `.PersonCard[data-id="${this.id}"]`;
+        TweenLite.set(cardClass, {y: 10}); // set the Y transform before animating it
+        this.$el.removeClass(CLASSES.IS_INACTIVE);
+        TweenLite.to(cardClass, 0.3, {y: 0});
     }
 
     // remove event listeners
