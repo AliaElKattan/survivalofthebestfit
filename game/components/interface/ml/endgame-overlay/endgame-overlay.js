24e44cfdc64040c03a56e99d6ec1c191a0d3476c
jihjihk <jihyun@nyu.edu>
2019-04-03 12:50:03 +0400

    game static files added

---
+++ game/components/interface/ml/endgame-overlay/endgame-overlay.js
@@ -0,0 +1,38 @@
+import $ from 'jquery';
+import EVENTS from '~/public/controllers/constants/events';
+import {TweenLite} from 'gsap/TweenMax';
+import CLASSES from '~/public/controllers/constants/classes';
+import {eventEmitter} from '~/public/controllers/game/gameSetup.js';
+
+export default class {
+    constructor(options) {
+        this.$el = $('#js-endgame-overlay');
+        this._addEventListeners();
+    }
+
+    _addEventListeners() {
+        eventEmitter.on(EVENTS.SHOW_ENDGAME_OVERLAY, this.show.bind(this));
+    }
+
+    _removeEventListeners() {
+        eventEmitter.off(EVENTS.SHOW_ENDGAME_OVERLAY, this.show.bind(this));
+    }
+
+    show() {
+        this.$el.removeClass(CLASSES.IS_INACTIVE);
+        TweenLite.set('#js-endgame-overlay', {y: 50, opacity: 0});
+        TweenLite.to('#js-endgame-overlay', 0.3, {y: 0, opacity: 1, ease: Power1.easeOut});
+    }
+
+    hide() {
+        TweenLite.to('#js-endgame-overlay', 0.2, {y: 20, opacity: 0, ease: Power1.easeOut});
+        TweenLite.delayedCall(0.4, () => {
+            this.$el.addClass(CLASSES.IS_INACTIVE);
+        });
+    }
+
+    destroy() {
+        this._removeEventListeners();
+        this.hide();
+    }
+}
---
+++ game/components/interface/ui-title/ui-title.js
@@ -0,0 +1,90 @@
+import $ from 'jquery';
+import CLASSES from '../../../controllers/constants/classes';
+import EVENTS from '../../../controllers/constants/events';
+
+import UIBase from '../ui-base/ui-base';
+import {eventEmitter} from '../../../controllers/game/gameSetup.js';
+
+export default class extends UIBase {
+    constructor(options) {
+        super();
+        this.options = options;
+        this.isAtSecondStage = false;
+        this.$el = $('.js-titlepage'); // This should be a single element
+        this.$headerEl = this.$el.find('.Titlepage__header');
+        this.$textEl = this.$el.find('.Titlepage__content');
+        this.$buttons = this.$el.find('.TextboxButton');
+        this._addEventListeners();
+        this.setContent = this.setContent.bind(this);
+        this._headerText = options.headerText;
+        this._mainContent = options.content; // TODO: change this to null
+        this._responseContent = options.responses || ['OK'];
+        if (options.show) this.show();
+        this.setContent(); // set content
+    }
+
+    setContent() {
+        this.$headerEl.html(this._headerText);
+        this.$textEl.html(this._mainContent);
+        this._responseContent.forEach((response, index) => {
+            const $responseButton = $(this.$buttons[index]);
+            $responseButton.removeClass(CLASSES.IS_INACTIVE);
+            $responseButton.find('.button__text').html(response);
+        });
+    }
+
+    updateContent(options) {
+        this.show();
+        this.$headerEl.html(options.headerText);
+        this.$textEl.html(options.content);
+        options.responses.forEach((response, index) => {
+            const $responseButton = $(this.$buttons[index]);
+            $responseButton.removeClass(CLASSES.IS_INACTIVE);
+            $responseButton.find('.button__text').html(response);
+        });
+    }
+
+    _buttonIsClicked(e) {
+        //TODO how to handle start button clicked vs share
+        this.$buttons.addClass(CLASSES.BUTTON_CLICKED);
+        if (!this.isAtSecondStage) {
+            eventEmitter.emit('first-start-button-clicked', {});
+            this.isAtSecondStage = true;
+            //this.hide();
+        }
+        else {
+            eventEmitter.emit('second-start-button-clicked', {});
+            this.destroy();
+        }
+    }
+
+    _addEventListeners() {
+        this.$buttons.click(this._buttonIsClicked.bind(this));
+    }
+
+    _removeEventListeners() {
+        // event listeners need to be removed explicitly because they are managed globally Jquery
+        this.$buttons.off();
+    }
+
+    show() {
+        this.$el.removeClass(CLASSES.IS_INACTIVE)
+            .removeClass(CLASSES.FADE_OUT)
+            .addClass(CLASSES.FADE_IN);
+    }
+
+    hide() {
+        this.$el.removeClass(CLASSES.FADE_IN)
+            .addClass(CLASSES.FADE_OUT)
+            .addClass(CLASSES.IS_INACTIVE);
+
+        // TODO you might need a delayed call for this
+    }
+
+    destroy() {
+        this._removeEventListeners();
+        super.dispose();
+        this.hide();
+        // this.$el.destroy();
+    }
+}
