f2bb5069edda2a2aee247a58f590007e4af9d004
jihjihk <jihyun@nyu.edu>
2019-05-11 19:26:28 +0800

    landing page and game merged

--- game/public/game/controllers/game/stateManager.js
+++ game/public/game/controllers/game/stateManager.js
@@ -23,9 +23,9 @@ const gameFSM = new machina.Fsm({
     states: {
         uninitialized: {
             startGame: function() {
-                // this.transition('titleStage');
+                this.transition('titleStage');
                 // this.transition('smallOfficeStage');
-                this.transition('mlTransitionStage');
+                // this.transition('mlTransitionStage');
                 // this.transition('mlLabStage');
             },
         },
