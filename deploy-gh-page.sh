f873f9fc76961aa868c37aa22e9525937255cd06
Gabor <gabor.csapo@nyu.edu>
2019-05-18 18:56:24 +0100

    more thorough version controlling of released versions

--- deploy-gh-page.sh
+++ deploy-gh-page.sh
@@ -8,6 +8,12 @@ read -p "Did you test your changes? Are you sure you want to push? [y/n]" -n 1 -
 echo    
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+    git checkout -b temp-release-branch
+    git add --force dist
+    git status 
+    git commit -m "gh pages release"
     git subtree push --prefix dist origin gh-pages
+    git checkout master
+    git branch -D temp-release-branch
 fi
 echo "Done. Make sure live page still works!"
