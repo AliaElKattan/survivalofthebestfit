1fd7dd4cb6fea4d6764d614d823d7a6a347ab05e
Alia ElKattan <aae322@nyu.edu>
2019-05-25 04:50:56 +0200

    open & close for resources headers

---
+++ website-source/js/resources.js
@@ -0,0 +1,33 @@
+//clean this code
+
+function open1() {
+	var x = document.getElementById("text1");
+		if (x.style.display === "none") {
+			x.style.display = "block";
+			console.log("")
+		} else {
+			x.style.display = "none";
+		}
+} 
+
+function open2() {
+	var y = document.getElementById("text2");
+		if (y.style.display === "none") {
+			y.style.display = "block";
+			console.log("")
+		} else {
+			y.style.display = "none";
+		}
+} 
+
+function open3() {
+	var z = document.getElementById("text3");
+		if (z.style.display === "none") {
+			z.style.display = "block";
+			console.log("")
+		} else {
+			z.style.display = "none";
+		}
+} 
+
+
