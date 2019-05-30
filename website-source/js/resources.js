ffdd5959f040e6010f3d020cd85e29a0f5c1ea47
Alia ElKattan <aae322@nyu.edu>
2019-05-30 23:47:04 +0200

    add resources page

---
+++ website-source/js/resources.js
@@ -0,0 +1,22 @@
+//clean this code
+
+function open1() {
+
+	var current = document.getElementById("text1");
+	var next = document.getElementById("text2");
+	
+	current.style.display = "none";
+	next.style.display = "block";
+	
+} 
+
+function open2() {
+
+	var current = document.getElementById("text2");
+	var next = document.getElementById("text3");
+	
+	current.style.display = "none";
+	next.style.display = "block";
+	
+} 
+
