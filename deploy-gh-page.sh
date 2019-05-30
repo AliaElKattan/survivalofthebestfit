018c0d09dd09a71676b8e35b241fdccf10e4b152
jihjihk <jihyun@nyu.edu>
2019-05-31 00:06:45 +0800

    merged final touches issues from jihyun-dev

--- deploy-gh-page.sh
+++ deploy-gh-page.sh
@@ -7,27 +7,31 @@ if [[ "$BRANCH" != "master" ]]; then
 fi
 
 read -p "Did you test your changes? Are you sure you want to push? [y/n]" -n 1 -r
-read -p "Is the state manager's stransition stage set to title? [y/n]" -n 1 -r
 echo    
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    echo "Rebuilding project"
-    cd website-source
-    npm run build
-    cd ../game-source
-    npm run build
-    cd ..
-    git add ./dist
-    git status 
-    read -p "Are you happy with the changelist? [y/n]" -n 1 -r
+    read -p "Is the state manager's stransition stage set to title? [y/n]" -n 1 -r
+    echo
     if [[ $REPLY =~ ^[Yy]$ ]]
     then
-        git commit -m "gh pages release"
-        git subtree split --prefix dist -b gh-pages
-        git push -f origin gh-pages:gh-pages
-    else
-        echo "Aborting and Resetting stage."
-        git reset
+        echo "Rebuilding project"
+        cd website-source
+        npm run build
+        cd ../game-source
+        npm run build
+        cd ..
+        git add ./dist
+        git status 
+        read -p "Are you happy with the changelist? [y/n]" -n 1 -r
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            git commit -m "gh pages release"
+            git subtree split --prefix dist -b gh-pages
+            git push -f origin gh-pages:gh-pages
+        else
+            echo "Aborting and Resetting stage."
+            git reset
+        fi
     fi
 fi
 echo "Done. Make sure live page still works!"
